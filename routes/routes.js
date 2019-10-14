59d5c12cdb19286155dfd2c29b2d46fbac0fb3f2
Willian Wallace <30538522+willianIF@users.noreply.github.com>
2019-10-14 17:08:19 -0300

    CRUD empresa CRUD parcial supervisor

--- routes/routes.js
+++ routes/routes.js
@@ -9,9 +9,130 @@ var appRouter = function (app) {
 
     });
 
-    app.get("/alunos", (req, res) => {
+    app.get("/supervisores", (req, res) => {
+        let sql = 'SELECT * FROM estagia.supervisor_view order by nome_supervisor;';
         conEstagia.getConnection(function (error, conEstagia) {
-            conEstagia.query('SELECT * FROM aluno order by nome_aluno;', function (error, results, fields) {
+            conEstagia.query(sql, function (error, results, fields) {
+                res.send(results);
+            });
+            conEstagia.release();
+        });
+    });
+
+    app.delete('/deleteSupervisor/:id', (req, res) => {
+        let sql = 'DELETE FROM supervisor WHERE idsupervisor =';
+        conEstagia.getConnection(function (err, conEstagia) {
+            conEstagia.query(sql + parseInt(req.params.id), (error, results, fields) => {
+                if (error)
+                    return console.error(error.message);
+                console.log('Deleted Row(s):', results.affectedRows);
+            });
+            conEstagia.release();
+        });
+    });
+
+    app.post('/inserirSupervisor', function (req, res) {
+        var nome = req.body.nome_empresa;
+        var CNPJ = req.body.CNPJ;
+        var endereco = req.body.endereco_empresa;
+        var bairro = req.body.bairro_empresa;
+        var email = req.body.email_empresa;
+        var telefone = req.body.telefone_empresa;
+
+        let sql = `INSERT INTO empresa (nome_empresa, email_empresa, endereco_empresa, telefone_empresa, CNPJ, bairro_empresa, idcidade_FK) VALUES ('${nome}', '${email}', '${endereco}', '${telefone}', '${CNPJ}', '${bairro}', '1')`;
+        conEstagia.getConnection(function (err, conEstagia) {
+            conEstagia.query(sql, function (err, result) {
+                if (err) throw err;
+                console.log("1 record inserted");
+            });
+            conEstagia.release();
+        });
+    });
+
+    app.put('/editarSupervisor/:id', function (req, res) {
+        var id = parseInt(req.params.id);
+        var nome = req.body.nome_empresa;
+        var CNPJ = req.body.CNPJ;
+        var endereco = req.body.endereco_empresa;
+        var bairro = req.body.bairro_empresa;
+        var email = req.body.email_empresa;
+        var telefone = req.body.telefone_empresa;
+
+        let sql = `UPDATE empresa SET nome_empresa = '${nome}', email_empresa = '${email}', endereco_empresa = '${endereco}', telefone_empresa = '${telefone}', CNPJ = '${CNPJ}', bairro_empresa = '${bairro}' WHERE idempresa = ` + id;
+        conEstagia.getConnection(function (err, conEstagia) {
+            conEstagia.query(sql, function (err, result) {
+            });
+            conEstagia.release();
+        });
+
+    });
+
+    // ----------------------------------------------------------------------------------
+
+    app.get("/empresas", (req, res) => {
+        let sql = 'SELECT * FROM empresa_view order by nome_empresa;';
+        conEstagia.getConnection(function (error, conEstagia) {
+            conEstagia.query(sql, function (error, results, fields) {
+                res.send(results);
+            });
+            conEstagia.release();
+        });
+    });
+
+    app.delete('/deleteEmpresa/:id', (req, res) => {
+        let sql = 'DELETE FROM empresa WHERE idempresa =';
+        conEstagia.getConnection(function (err, conEstagia) {
+            conEstagia.query(sql + parseInt(req.params.id), (error, results, fields) => {
+                if (error)
+                    return console.error(error.message);
+                console.log('Deleted Row(s):', results.affectedRows);
+            });
+            conEstagia.release();
+        });
+    });
+
+    app.post('/inserirEmpresa', function (req, res) {
+        var nome = req.body.nome_empresa;
+        var CNPJ = req.body.CNPJ;
+        var endereco = req.body.endereco_empresa;
+        var bairro = req.body.bairro_empresa;
+        var email = req.body.email_empresa;
+        var telefone = req.body.telefone_empresa;
+
+        let sql = `INSERT INTO empresa (nome_empresa, email_empresa, endereco_empresa, telefone_empresa, CNPJ, bairro_empresa, idcidade_FK) VALUES ('${nome}', '${email}', '${endereco}', '${telefone}', '${CNPJ}', '${bairro}', '1')`;
+        conEstagia.getConnection(function (err, conEstagia) {
+            conEstagia.query(sql, function (err, result) {
+                if (err) throw err;
+                console.log("1 record inserted");
+            });
+            conEstagia.release();
+        });
+    });
+
+    app.put('/editarEmpresa/:id', function (req, res) {
+        var id = parseInt(req.params.id);
+        var nome = req.body.nome_empresa;
+        var CNPJ = req.body.CNPJ;
+        var endereco = req.body.endereco_empresa;
+        var bairro = req.body.bairro_empresa;
+        var email = req.body.email_empresa;
+        var telefone = req.body.telefone_empresa;
+
+        let sql = `UPDATE empresa SET nome_empresa = '${nome}', email_empresa = '${email}', endereco_empresa = '${endereco}', telefone_empresa = '${telefone}', CNPJ = '${CNPJ}', bairro_empresa = '${bairro}' WHERE idempresa = ` + id;
+        conEstagia.getConnection(function (err, conEstagia) {
+            conEstagia.query(sql, function (err, result) {
+            });
+            conEstagia.release();
+        });
+
+    });
+
+    // ----------------------------------------------------------------------------------
+
+    app.get("/alunos", (req, res) => {
+        let sql = 'SELECT * FROM aluno order by nome_aluno;';
+        conEstagia.getConnection(function (error, conEstagia) {
+            conEstagia.query(sql, function (error, results, fields) {
                 res.send(results);
             });
             conEstagia.release();
@@ -19,8 +140,9 @@ var appRouter = function (app) {
     });
 
     app.delete('/deleteAluno/:id', (req, res) => {
+        let sql = 'DELETE FROM aluno WHERE idaluno =';
         conEstagia.getConnection(function (err, conEstagia) {
-            conEstagia.query('DELETE FROM aluno WHERE idaluno =' + parseInt(req.params.id), (error, results, fields) => {
+            conEstagia.query(sql + parseInt(req.params.id), (error, results, fields) => {
                 if (error)
                     return console.error(error.message);
                 console.log('Deleted Row(s):', results.affectedRows);
@@ -66,9 +188,6 @@ var appRouter = function (app) {
         var telefone = req.body.telefone_aluno;
         var periodo = req.body.periodo_aluno;
         var matricula = req.body.matricula_aluno;
-
-        data_nascimento = data_nascimento.split("-");
-        data_nascimento = data_nascimento.join("-");
 
         let sql = `UPDATE aluno SET nome_aluno = '${nome}', email_aluno = '${email}', endereco_aluno = '${endereco}', telefone_aluno = '${telefone}', data_nascimento_aluno = '${data_nascimento}', CPF_aluno = '${CPF}', bairro_aluno = '${bairro}', matricula_aluno = '${matricula}', periodo_aluno = '${periodo}', nome_orientador_aluno = '${nome_orientador}' WHERE idaluno = ` + id;
         conEstagia.getConnection(function (err, conEstagia) {
